341a5852d0772bee10eeb2a2b5d2613158f3318b
Antonin Béthoux bethouxa <bethouxa+git@outlook.fr>
2020-01-06 02:27:41 +0100

    More pre-git repo work

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Realtime, HTTP log centralizer for Eve Online.
 
 Fetches and sends fleet logs/game logs to a centralized server for fleet analytics (precise features TBD).
+
+Probably not EULA compliant, for educational purposes only I guess.
---
+++ server.ps1
@@ -0,0 +1,19 @@
+$ipaddr = (Invoke-WebRequest ipecho.net/plain) -split "\n" | select -last 1
+write-host "internet ip is $ipaddr"
+
+$ipaddr = Get-NetIPAddress | ?{$_.IPAddress -like "10.*" -or $_.IPAddress -like "192.168.*"} | select -exp IPAddress
+write-host "local ip is $ipaddr"
+
+$listener = New-Object System.Net.HttpListener
+$listener.Prefixes.Add("http://*:53000/")
+$listener.Start()
+
+while($true) {
+    $context = $listener.GetContext()
+    Start-Job -ScriptBlock {Handle($args)} -ArgumentList $res
+}
+
+function Handle($HTTPContext)
+{
+    
+}
